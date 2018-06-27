4f481a600ac229549ba8c98430bd32f432fb65a1
abielbrhane <35551756+abielbrhane@users.noreply.github.com>
2018-06-26 22:12:50 -0600

    html and js files for world cup results

---
+++ script1.js
@@ -0,0 +1,63 @@
+const{ fromEvent, from,of }=rxjs;
+const{map,debounceTime,filter}=rxjs.operators;
+
+const data=fetch('https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json');
+
+let arrayOfTeams=[];
+
+             data.then(data=>data.json())
+              .then(data => {
+                  const allTeams=data.teams;
+
+                  from(allTeams)
+                                    .pipe(
+                                        filter(team => team.id <5)
+                                    ).subscribe(team => 
+                                       arrayOfTeams.push(team)
+                                    )      
+              from(arrayOfTeams)
+              .pipe(
+                  map(singleTeam => singleTeam)
+              )
+              .subscribe(singleTeam => {
+                  $('#tableBody')
+                      .append($('<tr>')
+                              .append($('<td>')
+                                      .append($('<div>')
+                                              .append($('<img>', {
+                                                      'src': singleTeam.flag,
+                                                      'height': '20px',
+                                                      'width': '30px'
+                                                  })
+                                              )
+                                              .append($('<span>', {
+                                                      'text': " " + singleTeam.name
+                                                  })
+                                              )
+                                      )
+                              )
+                      )
+              })
+      
+            })
+            .catch((err) => {console.error(err);})
+
+    var input = fromEvent(document.querySelector('input'), 'input');
+
+    input
+        .pipe(
+      
+            debounceTime(200),
+            map(event => event.target.value)
+        )
+        .subscribe(value => {
+            if(value){
+                value = value.toLowerCase();
+                $("#tableBody tr").filter(function() {
+                    console.log($(this).text().toLowerCase());
+                    
+                  $(this).toggle($(this).text().toLowerCase().indexOf(value) === 1)
+                });
+            }
+        });
+    
